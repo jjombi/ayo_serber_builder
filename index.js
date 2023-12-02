d77bf7c600bccb3c684f6b13f7a4d1ab41fa1843
jjjombi <sis01066745950@gmail.com>
2023-12-02 20:14:40 +0900

    ayo + ayo2

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require('express')
 const cors = require('cors');
 const mysql = require('mysql');
 const mariadb = require('mariadb');
+const multer  = require('multer');
 const body_parser = require("body-parser");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const saltRounds = 5;
-
+const fs = require('fs');
+const { title } = require('process');
+const fileUpload = require('express-fileupload');
 const port = 45509;
 const app = express()
 app.use(body_parser.json());
+app.use(
+  body_parser.urlencoded({
+    extended: true,
+  })
+);
+app.use(fileUpload());
 app.use(cors({
   // origin : "https://ay0.netlify.app",
   // origin: "https://jjombi.github.io",
@@ -39,7 +48,13 @@ const pool = mariadb.createPool({
   port: 32325,
   database: 'ayodb',
 });
-
+const pool_main = mariadb.createPool({  // main2 db
+  host   : 'svc.sel5.cloudtype.app',
+  user   : 'root', 
+  password: 'sis01066745950@', 
+  port: 32325,
+  database: 'ayo_main',
+});
 
 // /*--------------------------------------------------------------*/
 
@@ -366,10 +381,336 @@ app.post('/vote',cors({
 
 
 })
+const search_roomName = () => {
+  pool_main.getConnection()
+    .then((conn)=>{
+      conn.query(`select roomName from queze ORDER BY roomName DESC LIMIT 1;`).then((result)=>{
+        if(result.length != 0){
+          let roomName_arr = Array.from(result[0].roomName);// ['A','B','C']; 
+            if(roomName_arr[roomName_arr.length - 1].charCodeAt() >= 90)
+          { 
+            roomName_arr.push(String.fromCharCode(65));
+            return(roomName_arr);
+          }
+          else
+          { 
+            roomName_arr[roomName_arr.length - 1] =  String.fromCharCode(roomName_arr[roomName_arr.length - 1].charCodeAt() + 1);
+            return(roomName_arr);
+          }
+        }
+        else {
+          return('A');
+        }     
+      })
+    })
+  
+}
+let callbackExecuted = false;
+
+const storage = multer.diskStorage({
+  
+  destination: function (req, file, cb) {
+    console.log('stroag : 사진 저장 함수',req.body,file);
+    console.log('1',req.body);
+    pool_main.getConnection()
+    .then((conn)=>{
+      console.log('2',req.body);
+      conn.query(`select roomName from queze ORDER BY roomName DESC LIMIT 1;`).then((result)=>{
+        if(result.length != 0){
+          console.log('3',req.body);
+          let roomName_arr = Array.from(result[0].roomName);// ['A','B','C']; 
+            if(roomName_arr[roomName_arr.length - 1].charCodeAt() >= 90)
+          { 
+            roomName_arr.push(String.fromCharCode(65));
+            cb_(roomName_arr,cb);
+            if(!callbackExecuted) upload_query(req,roomName_arr); callbackExecuted = false;
+          }
+          else
+          { 
+            console.log('4',req.body);
+            roomName_arr[roomName_arr.length - 1] =  String.fromCharCode(roomName_arr[roomName_arr.length - 1].charCodeAt() + 1);
+            cb_(roomName_arr,cb);
+            if(!callbackExecuted){ 
+              console.log('5',req.body);
+              conn.query(`select * from queze where roomName = '${roomName_arr}';`).then(result=>{ // 수정 필요
+                console.log('6',req.body);
+                console.log('444 result',result,req.body);
+                if(result.length === 0){
+                  console.log('7',req.body);
+                  conn.query(`insert into queze (roomName, existence, title, title_img_name) value('${roomName_arr}', 1, '${req.body.title}', '${req.body.img_name[0]}');`);
+                  for(let i=0;i<req.body.text.length;i++){
+                    conn.query(`insert into result (text, value, originalname, roomName) value('${req.body.text[i]}', 0, '${req.body.title}','${roomName_arr}')`);
+                  }
+                }
+              })  
+              callbackExecuted = false;
+            }
+            
+          }
+        }
+        else {
+          cb_(roomName_arr,cb);
+          if(!callbackExecuted) upload_query(req,roomName_arr); callbackExecuted = false;
+        }     
+      })
+    })
+        
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname+'.jpg');
+  }
+});
+const cb_ = (roomName_arr,cb) => {
+  const dir = `C:/Users/user/OneDrive/문서/GitHub/ayo_serber_builder/uploads/${roomName_arr}`;
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+  cb(null, `uploads/${roomName_arr}`);
+}
+const ex_img_name = ['img1',"img2","img3","img4","img5","img6","img7","img8","img9"];
+const upload_query = async (req, roomName_arr) =>{
+  console.log('upload query 시작 req : ',req.body,roomName_arr);
+ 
+  pool_main.getConnection().then((conn)=>{
+    conn.query(`select * from queze where roomName = '${roomName_arr}';`).then(result=>{ // 수정 필요
+      console.log('444 result',result,req);
+      if(result.length === 0){
+        if(req.body.length === undefined){
+          conn.query(`insert into queze (roomName, existence, title, title_img_name) value('${roomName_arr}', 1, '${req.body.title}', 'img0.jpg');`);
+          conn.query(`insert into result (text, value, originalname, roomName) value('${req.body.text}', 0, 'img0.jpg','${roomName_arr}')`);
+        }
+        else{
+          for(i=0 ; i < req.body.length ;i++){
+            conn.query(`insert into queze (roomName, existence, title, title_img_name) value('${roomName_arr}', 1, '${req.body[i].title}', 'img${i}.jpg');`);
+            conn.query(`insert into result (text, value, originalname, roomName) value('${req.body[i].text}', 0, 'img${i}.jpg','${roomName_arr}')`);
+          }
+        }
+        conn.query(`create table ${roomName_arr}_comments (value varchar(40), parent_room_num int, likes int, type tinyint);`)
+      }
+    })  
+  })  
+  
+}
+
+const upload = multer({ storage : storage }).array('img'); 
+
+// let callbackExecuted = false;
+app.use(body_parser.urlencoded({ extended: true }));
+
+app.post('/upload_img',(req,res)=>{
+  console.log('upload img 시작',req.body,req.files,req.file); //req.files.img[0].name or data(type BUffer)
 
 
+  // if(!callbackExecuted) {
+    pool_main.getConnection()
+    .then((conn)=>{
+      conn.query(`select roomName from queze ORDER BY roomName DESC LIMIT 1;`).then((result)=>{
+        if(result.length != 0){
+          let roomName_arr = Array.from(result[0].roomName);// ['A','B','C']; 
+            if(roomName_arr[roomName_arr.length - 1].charCodeAt() >= 90)
+          { 
+            roomName_arr.push(String.fromCharCode(65));
+            const dir = `C:/Users/user/OneDrive/문서/GitHub/ayo_serber_builder/uploads/${roomName_arr}`;
+            if (!fs.existsSync(dir)) {
+              fs.mkdirSync(dir, { recursive: true });
+            }
+            if(req.files.img.length === undefined) { // img is one
+              req.files.img.mv(dir+`/img0`+'.jpg');
+              upload_query(req,roomName_arr)
+
+            }
+            else {
+              let i = 0;
+              req.files.img.map(e=>{ // img가 하나 일때 예외 수정
+                e.mv(dir+`/img`+i+'.jpg');
+                i++; // 'img'+i
+              })
+              upload_query(req,roomName_arr)
+            }
+          }
+          else
+          { 
+            roomName_arr[roomName_arr.length - 1] =  String.fromCharCode(roomName_arr[roomName_arr.length - 1].charCodeAt() + 1);
+            const dir = `C:/Users/user/OneDrive/문서/GitHub/ayo_serber_builder/uploads/${roomName_arr}`;
+            console.log(typeof(req.files.img),req.files.img.length);
+            if (!fs.existsSync(dir)) {
+              fs.mkdirSync(dir, { recursive: true });
+            }
+            if(req.files.img.length === undefined) { // img is one
+              req.files.img.mv(dir+`/img0`+'.jpg');              
+              upload_query(req,roomName_arr)
+
+            }
+            else {
+              let i = 0;
+              req.files.img.map(e=>{ // img가 하나 일때 예외 수정
+                e.mv(dir+`/img`+i+'.jpg'); 
+                i++; // 'img'+i
+              })
+              upload_query(req,roomName_arr)
+            }
+          }
+        }
+        else {
+          const dir = `C:/Users/user/OneDrive/문서/GitHub/ayo_serber_builder/uploads/A`;
+          if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+          }
+          if(req.files.img.length === undefined) { // img is one
+            req.files.img.mv(dir+`/img0`+'.jpg');
+            upload_query(req,'A');
+          }
+          else {
+            let i = 0;
+            req.files.img.map(e=>{ // img가 하나 일때 예외 수정
+              e.mv(dir+`/img`+i+'.jpg');
+              i++; // 'img'+i
+            })
+            upload_query(req,'A');
+          }      
+        }     
+      })
+    })
+    callbackExecuted = true;
+  // }
+
+  res.send('seccess');
+  // res.redirect('http://localhost:8080/main');
+  
+})
+// existence 존재 
 //-------------------------------------------------------------------
+app.get('/main_select_queze',(req,res)=>{ //main 페이지 대표 사진과 제목 보냄 queze desc 변경 후 수정 /할 일
+  pool_main.getConnection().then((conn)=>{
+    conn.query(`select * from queze where existence = 1`).then((result)=>{
+      console.log(result);
+      let base64_img_arr = [];
+      result.map(e=>{
+        base64_img_arr = [...base64_img_arr,(fs.readFileSync(`uploads/${e.roomName}/${e.title_img_name}`).toString('base64'))];
+      });
+      console.log('a-typr',base64_img_arr);
+      return res.set({ "Content-Type": 'mulipart/form-data'}).send({result : result, base64_img_arr : base64_img_arr });
+    })
+  })
+})
+app.post('/main_a_queze',(req,res)=>{
+  const roomName = req.body.roomName;
+  let   text_arr = [];
+  let   img_arr  = [];
+  pool_main.getConnection().then((conn)=>{
+    conn.query(`select * from result where roomName='${roomName}'`).then(result=>{
+      console.log('select from result whee roomName=',roomName,result);
+      result.map(e=>{
+        text_arr = [...text_arr,e.text];
+        img_arr = [...img_arr,(fs.readFileSync(`uploads/${roomName}/`+e.originalname).toString('base64'))];
+      })
+    }).then(()=>{
+      console.log('text_arr',text_arr,img_arr); // text arr [queze_length,text1,text2,text3]
+      return res.send({text : text_arr, img : img_arr});
+    })  
+  })  
 
+
+})
+// comments desc : create table $roomName_comments (value varchar(40), parent_room_num int, likes int, type tinyint); parent_room_num 1,2,3 type : 0=자식, 1=부모
+app.post('/main_a_queze_comments',(req,res)=>{ // url 파라미터로 roomName 가져오게 바꾸기
+  pool_main.getConnection().then((conn)=>{
+    conn.query(`select * from ${req.body.roomName}_comments where type = 1`).then((result)=>{
+      console.log('result',result);
+      return res.send(result);
+    })
+    
+  })
+})
+app.post('/main_a_queze_children_comments',(req,res)=>{
+  pool_main.getConnection().then((conn)=>{
+    conn.query(`select * from ${req.body.roomName}_comments where parent_room_num = ${req.body.parent_room_num} && type = 0 order by likes desc`).then(result=>{
+      return res.send(result);
+    })  
+  })
+})
+app.post('/main_a_queze_plus_comments',(req,res)=>{
+
+  pool_main.getConnection().then((conn)=>{
+    // 부모일때
+    console.log('댓 추가 req.body',req.body);
+    const roomName = req.body.roomName;
+    const type = req.body.type;
+    const value = req.body.value;
+    if(type === 1){
+      conn.query(`select parent_room_num from ${roomName}_comments where type = 1 order by parent_room_num desc;`).then(result=>{
+        console.log('댓글 type 1dml room_num 가져온 걀과 result : ',result);
+        if(result.length === 0){
+          conn.query(`insert into ${roomName}_comments (value, parent_room_num, likes, type) value('${value}',0,0,1)`).then((result)=>{
+            return res.send('success');
+          })
+        }
+        else {
+          const new_parent_room_num = result[0].parent_room_num + 1;
+          console.log('new_parent_room_num',new_parent_room_num);
+          conn.query(`insert into ${roomName}_comments (value, parent_room_num, likes, type) value('${value}',${new_parent_room_num},0,1)`).then((result)=>{
+            return res.send('success');
+          })
+        }
+      })
+
+    }
+    else{
+      conn.query(`insert into ${roomName}_comments (value, parent_room_num, likes, type) value('${value}',${type},0,0)`).then((result)=>{
+        return res.send('success');
+      })
+    }
+  })
+})
+app.post('/result_plus',(req,res)=>{
+
+  const roomName = req.body.roomName;
+  const column_name_arr = req.body.column_name; // [[txt1,3],[txt3,2],[txt2,1]]
+  let insert_content = '';
+  let insert_value = '';
+  // column_name_arr.map(e=>{
+  //   e[0]
+  //   insert_content += 
+  //   insert_value +=
+  // })
+  pool_main.getConnection().then((conn)=>{
+    conn.query(`select * from result where roomName = '${roomName}'`).then((result)=>{
+      console.log('selct * from result where roomName',result); //[ { text: 'ㅌㅇㅌ1', value: 0 }, { text: 'ㅌㅇㅌ2', value: 0 }, { text: 'ㅌㅇㅌ3', value: 0 }]
+      result.map(result_e=>{
+        column_name_arr.map(res_e=>{
+          if(result_e.text === res_e[0]){
+            conn.query(`update result set value = ${Number(result_e.value) + res_e[1]} where text = '${result_e.text}'`);
+          }
+        })
+      })
+    }).then(()=>{
+      return( res.send('success'));
+    })
+
+  })
+
+})
+app.post('/main_result',(req,res)=>{
+  const roomName = req.body.roomName;
+  
+  pool_main.getConnection().then((conn)=>{
+    conn.query(`select * from result where roomName = '${roomName}' order by value desc;`).then(result=>{
+      console.log(result);
+      const send_ = result.map(e=>{
+        return {
+          img : fs.readFileSync(`uploads/${roomName}/${e.originalname}`).toString('base64'),
+          text : e.text,
+          value : e.value
+        }
+      })
+      return res.send(send_);
+    })
+  })
+})
+// app.post('/select_title_text',(req,res)=>{
+//   const roomName_arr = req.body.roomName; // [A,B,C]
+//   pool_main.getConnection(`select * from ${roomNameo}`)
+// })
 app.listen(port, (err) => {
   console.log(`Example app listening on port ${port}`)
   console.log(err);
