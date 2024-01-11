bc40dad95f8c66132a00cc92cca986fff34c0585
jjjombi <sis01066745950@gmail.com>
2024-01-11 21:42:51 +0900

    modify update
    이미지 추가 할때 이미지 여러개일 경우 이미지 명을 잘못 줌 img0.img,
    lastnum + i + 1 함

--- index.js
+++ index.js
@@ -247,12 +247,12 @@ app.post('/upload_img_plus',(req,res)=>{
   const roomName = req.body.roomName;
   const last_num = req.body.last_num;
   console.log('upload img plus 시작',req.body,typeof(req.body.text));
-  if(typeof(req.body.text) !== "string"){
+  if(typeof(req.body.text) !== "string"){ // 배열일 경우 이미지가 여러개 일 때
     console.log('typeof(req.body.text) !== "string"');
     for(i=0 ; i < req.body.img_name.length ;i++){
       console.log(i);
       if(req.body.text[i] === undefined || req.body.text[i] === '') connection.query(`insert into result (text, value, uuid, originalname, roomName) value('', 0, '${uuidv4()}', 'img${last_num+i+1}.jpg','${roomName}')`);
-      else connection.query(`insert into result (text, value, uuid, originalname, roomName) value('${req.body.text[i]}', 0, '${uuidv4()}', 'img${i}.jpg','${roomName}')`);
+      else connection.query(`insert into result (text, value, uuid, originalname, roomName) value('${req.body.text[i]}', 0, '${uuidv4()}', 'img${Number(last_num)+i+1}.jpg','${roomName}')`);
     }
   }else{
     console.log('type str');
