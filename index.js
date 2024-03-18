db79d84a9db601aa70260350906acfc5b5c933b3
jjjombi <sis01066745950@gmail.com>
2024-03-18 15:12:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -877,7 +877,7 @@ app.post('/make_quezeshow',(req,res)=>{ //나락퀴즈 문제 만들기
     // }else{
       if(typeof(img_tinyint) === 'string'){
         console.log('img_tinyint === str');
-        if(img_tinyint === true){
+        if(img_tinyint === 'true'){
           console.log(img_tinyint,'img_tinyint === true');
           connection.query(`insert into quezeshowqueze (uuid, title, existence, date, likes, img, roomnum, password, explainText, quezeshow_type, queze_type) value('${uuid}', '${queze_title}', 1, ${date}, 0, '0.jpg', ${result_roomnum + 1}, '${modify_password}', '${quezeshowqueze_explain_text}', '${quezeshow_type}', ${queze_type_})`,(err,result)=>{
             if(err){
@@ -895,7 +895,7 @@ app.post('/make_quezeshow',(req,res)=>{ //나락퀴즈 문제 만들기
       }else {
         console.log('img_tinyint is arr',img_tinyint);
         for(let i = 0 ; i <= img_tinyint.length; i++){
-          if(img_tinyint[i] === true){
+          if(img_tinyint[i] === 'true'){
             console.log('img_tinyint i === true',img_tinyint,i);
             connection.query(`insert into quezeshowqueze (uuid, title, existence, date, likes, img, roomnum, password, explainText, quezeshow_type, queze_type) value('${uuid}', '${queze_title}', 1, ${date}, 0, '${i}.img', ${result_roomnum + 1}, '${modify_password}', '${quezeshowqueze_explain_text}', '${quezeshow_type}', ${queze_type_})`,(err,result)=>{
               if(err){
