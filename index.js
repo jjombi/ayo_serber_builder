06205354c29a2311f7d9a4d17e4a0497857ca17c
jjjombi <sis01066745950@gmail.com>
2024-04-02 17:12:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ app.use(cors({
   methods : '*',
 }))
 AWS.config.update({
-  accessKeyId: 'process.env.AWS_ACCESS_KEY_ID',
-  secretAccessKey: 'process.env.AWS_SECRET_ACCESS_KEY',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'ap-northeast-2'
 });
 const client = new S3Client(
