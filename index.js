346c5c2afcf6ec233f6f02613a750402dd799f78
jjjombi <sis01066745950@gmail.com>
2024-04-11 18:05:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -985,54 +985,54 @@ app.post('/make_quezeshow',(req,res)=>{ //나락퀴즈 문제 만들기
     //     }
     //   })
     // }else{
-      if(typeof(img_tinyint) === 'string'){
-        console.log('img_tinyint === str');
-        if(img_tinyint === 'true'){
-          console.log(img_tinyint,'img_tinyint === true');
-          connection.query(`insert into quezeshowqueze (uuid, title, existence, date, likes, img, roomnum, password, explainText, quezeshow_type) value('${uuid}', '${queze_title}', 1, ${date}, 0, '0.jpg', ${result_roomnum + 1}, '${modify_password}', '${quezeshowqueze_explain_text}', '${quezeshow_type}')`,(err,result)=>{
-            if(err){
-              throw err
-            }
-          })
-        }else {
-          console.log(img_tinyint,'img_tinyint === flase');
-          connection.query(`insert into quezeshowqueze (uuid, title, existence, date, likes, img, roomnum, password, explainText, quezeshow_type) value('${uuid}', '${queze_title}', 1, ${date}, 0, '', ${result_roomnum + 1}, '${modify_password}', '${quezeshowqueze_explain_text}', '${quezeshow_type}')`,(err,result)=>{
-            if(err){
-              throw err
-            }
-          })
-        }
-      }else {
-        console.log('img_tinyint is arr',img_tinyint);
-        for(let i = 0 ; i <= img_tinyint.length; i++){
-          if(img_tinyint[i] === 'true'){
-            console.log('img_tinyint i === true',img_tinyint,i);
-            connection.query(`insert into quezeshowqueze (uuid, title, existence, date, likes, img, roomnum, password, explainText, quezeshow_type) value('${uuid}', '${queze_title}', 1, ${date}, 0, '${i}.jpg', ${result_roomnum + 1}, '${modify_password}', '${quezeshowqueze_explain_text}', '${quezeshow_type}')`,(err,result)=>{
-              if(err){
-                throw err
-              }
-            })
-            i=img_tinyint.length+1;
-          }else{
-            console.log('전부다 false',img_tinyint,i);
-            if(i === img_tinyint.length){
-              connection.query(`insert into quezeshowqueze (uuid, title, existence, date, likes, img, roomnum, password, explainText, quezeshow_type) value('${uuid}', '${queze_title}', 1, ${date}, 0, '', ${result_roomnum + 1}, '${modify_password}', '${quezeshowqueze_explain_text}', '${quezeshow_type}')`,(err,result)=>{
-                if(err){
-                  throw err
-                }
-              })
-            }
-          }     
-        }
-      // }
+    //   if(typeof(img_tinyint) === 'string'){
+    //     console.log('img_tinyint === str');
+    //     if(img_tinyint === 'true'){
+    //       console.log(img_tinyint,'img_tinyint === true');
+    //       connection.query(`insert into quezeshowqueze (uuid, title, existence, date, likes, img, roomnum, password, explainText, quezeshow_type) value('${uuid}', '${queze_title}', 1, ${date}, 0, '0.jpg', ${result_roomnum + 1}, '${modify_password}', '${quezeshowqueze_explain_text}', '${quezeshow_type}')`,(err,result)=>{
+    //         if(err){
+    //           throw err
+    //         }
+    //       })
+    //     }else {
+    //       console.log(img_tinyint,'img_tinyint === flase');
+    //       connection.query(`insert into quezeshowqueze (uuid, title, existence, date, likes, img, roomnum, password, explainText, quezeshow_type) value('${uuid}', '${queze_title}', 1, ${date}, 0, '', ${result_roomnum + 1}, '${modify_password}', '${quezeshowqueze_explain_text}', '${quezeshow_type}')`,(err,result)=>{
+    //         if(err){
+    //           throw err
+    //         }
+    //       })
+    //     }
+    //   }else {
+    //     console.log('img_tinyint is arr',img_tinyint);
+    //     for(let i = 0 ; i <= img_tinyint.length; i++){
+    //       if(img_tinyint[i] === 'true'){
+    //         console.log('img_tinyint i === true',img_tinyint,i);
+    //         connection.query(`insert into quezeshowqueze (uuid, title, existence, date, likes, img, roomnum, password, explainText, quezeshow_type) value('${uuid}', '${queze_title}', 1, ${date}, 0, '${i}.jpg', ${result_roomnum + 1}, '${modify_password}', '${quezeshowqueze_explain_text}', '${quezeshow_type}')`,(err,result)=>{
+    //           if(err){
+    //             throw err
+    //           }
+    //         })
+    //         i=img_tinyint.length+1;
+    //       }else{
+    //         console.log('전부다 false',img_tinyint,i);
+    //         if(i === img_tinyint.length){
+    //           connection.query(`insert into quezeshowqueze (uuid, title, existence, date, likes, img, roomnum, password, explainText, quezeshow_type) value('${uuid}', '${queze_title}', 1, ${date}, 0, '', ${result_roomnum + 1}, '${modify_password}', '${quezeshowqueze_explain_text}', '${quezeshow_type}')`,(err,result)=>{
+    //             if(err){
+    //               throw err
+    //             }
+    //           })
+    //         }
+    //       }     
+    //     }
+    //   // }
       
-    }
+    // }
     if(quezeshow_type === 'vote'){
       make_quezeshow_query_type_vote(uuid,content_title,explain_text,result_roomnum,img_tinyint);
     }
     else if(quezeshow_type === 'multiple'){// queze type 문제 생성 
       const choice = req.body.choice;
-      const answer = req.body.answer;
+      const answer = req.body.correct_choice;
       console.log('choice',choice,'answer',answer);
       // make_quezeshow_query_type_queze(uuid,content_title,explain_text,result_roomnum,choice,answer,img_tinyint);
     }
