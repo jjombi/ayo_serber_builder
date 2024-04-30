9ad31fbc3703c07d1455c9e6771f9aec07025276
jjjombi <sis01066745950@gmail.com>
2024-04-30 09:23:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ app.get('/modify_get_img_i',(req,res)=>{// db img에 i 값 구하기
   const uuid = req.query.uuid;
   connection.query(`select * from quezeshowcontent where uuid = '${uuid}';`,(err,result)=>{
     // res.send(result.length);
-    res.status(200).send((results.length).toString());
+    res.status(200).send((result.length).toString());
   })
 })
 // app.post('/modify_queze',(req,res)=>{ // queze 수정 전 데이터 받기
