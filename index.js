c614959a727ce290c36cdd1d7a84feca2267a0bc
jjjombi <sis01066745950@gmail.com>
2024-05-06 22:11:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1162,14 +1162,14 @@ app.get('/declaration',(req,res)=>{
   const transporter = nodemailer.createTransport({
     service: 'gmail', // gmail을 사용함
     auth: {
-      user: 'sis01066745950@gmail.com', // 나의 (작성자) 이메일 주소
+      user: 'jigun8171@gmail.com', // 나의 (작성자) 이메일 주소
       pass: 'dlworjsdmsWkd' // 이메일의 비밀번호
     }
   });
   
   const mailOptions = {
-    from: 'youremail@gmail.com', // 작성자
-    to: 'myfriend@yahoo.com', // 수신자
+    from: 'jigun8171@gmail.com', // 작성자
+    to: 'jigun8171@gmail.com', // 수신자
     subject: `declaration(roomnum:${roomnum})`, // 메일 제목
     text: `
       roomnum : ${roomnum}
