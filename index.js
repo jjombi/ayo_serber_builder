14213f6d11d652cf0faf08fddab9b7c26fcbd18f
jjjombi <sis01066745950@gmail.com>
2024-06-06 15:08:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -978,7 +978,7 @@ app.get('/search_quezeshow',(req,res)=>{
           roomnum : e.roomnum,
           quezeshow_type : e.quezeshow_type,
           explain_text : e.explainText,
-          likes_queze : e.likes_queze || null
+          likes_queze : e.likes_queze !== null && e.likes_queze !== undefined ? true : false
         }
         console.log('send message 만들어 자는 중 ');
       }
