6a5ba9bb7463e250d4f2a3bab664c81b97416f90
jjjombi <sis01066745950@gmail.com>
2024-07-21 01:49:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1000,7 +1000,7 @@ const search_query_func = (query,res) => {
 }
 app.get('/get_all_quezeshow',(req,res)=>{
   connection.query(`select * from quezeshowqueze where existence = 1`,(err,result)=>{
-    console.log('ressssss',res);
+    console.log('ressssss',result);
     return res.send(result);
   })
 })
