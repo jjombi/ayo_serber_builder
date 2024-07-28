514b30bbee2ed60c2be7432d6f03996ec5e1cf68
jjjombi <sis01066745950@gmail.com>
2024-07-28 07:47:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -921,7 +921,7 @@ app.post('/add_quezeshowcontent',(req,res)=>{
         console.log(i,'.e',e);
         const uuid2 = uuidv4();
         if(quezeshow_type === 'multiple'){// queze type 문제 생성 
-          connection.query(`insert into correct_choice (uuid, correct_choice) value('${uuid2}', '${e.correct_choice}')`)
+          connection.query(`insert into correct_choice (uuid, correct_choice) value('${uuid2}', '${e.correct_choice.replace(/'/g,"\\'").replace(/"/g,'\\"')}')`);
           e.choice.map((ev,i)=>{
             connection.query(`insert into choice (uuid, choice) value('${uuid2}','${ev}')`);
           })      
